d2c9233aacc3d253aac2abaa7f558b2c54158185
adriano <adrianolinhares85@gmail.com>
2014-12-11 14:44:39 -0200

    a

---
+++ NossosGastos/Registrator.cs
@@ -0,0 +1,26 @@
+﻿using NossosGastos.Abstratos;
+using NossosGastos.EFContext;
+using SimpleInjector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NossosGastos
+{
+    public static class Registrator
+    {
+        public static void Register(Container container)
+        {
+            Repositories(container);
+        }
+
+        public static void Repositories(Container container)
+        {
+            container.Register<ICompraRepository,EFCompraRepository>();
+            container.Register<IPagamentoRepository, EFPagamentoRepository>();
+            container.Register<IFormaPagamentoRepository, EFFormaPagamentoRepository>();
+        }
+
+    }
+}
