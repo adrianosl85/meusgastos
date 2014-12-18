aa8a019956937f6f2324255b828dd99bbd14f1c8
adriano <adrianolinhares85@gmail.com>
2014-12-18 07:48:58 -0200

    a

---
+++ Expressao/Expressao.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Linq;
+
+namespace Expressao
+{
+    public class Expressao<T> : IExpressao<T> where T:class
+    {
+        private Expression<Func<T, bool>> esquerda;
+
+        public void Criar(Expression<Func<T, bool>> esquerda)
+        {
+            this.esquerda = esquerda;
+        }
+
+        public void E(Expression<Func<T, bool>> direita)
+        {
+            this.esquerda = this.esquerda.And(direita);
+        }
+
+        public void Ou(Expression<Func<T, bool>> direita)
+        {
+            this.esquerda = this.esquerda.Or(direita);
+        }
+
+        public Func<T, bool> Compilar()
+        {
+            return esquerda.Compile();
+        }
+
+    }
+}
---
+++ Expressao/IExpressao.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expressao
+{
+    public interface IExpressao<T> where T:class
+    {
+        void E(Expression<Func<T, bool>> direita);
+
+        void Ou(Expression<Func<T, bool>> direita);
+
+        void Criar(Expression<Func<T, bool>> direita);
+
+        Func<T, bool> Compilar();
+    }
+}
